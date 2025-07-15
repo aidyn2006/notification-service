1047a5ff6a64b2c5e54e36fb3bb0af84540f8e20
a.nurlan <a.nurlan@documentolog.com>
2025-07-15 15:05:47 +0500

    Adapters and notification service

---
+++ src/main/java/com/example/notificationservice/notification/service/PhoneNotificationAdapter.java
@@ -0,0 +1,12 @@
+package com.example.notificationservice.notification.service;
+
+import com.example.notificationservice.notification.NotifyAdapter.Notification;
+import com.example.notificationservice.notification.NotifyAdapter.Notify;
+
+public class PhoneNotificationAdapter implements Notify {
+
+    @Override
+    public void send(Notification notification) {
+
+    }
+}
