9eedab084448d3650d30539398fc10499fbb2914
a.nurlan <a.nurlan@documentolog.com>
2025-07-16 16:49:29 +0500

    Listener rabbit

--- src/main/java/com/example/notificationservice/notification/config/RabbitMqConfig.java
+++ src/main/java/com/example/notificationservice/notification/config/RabbitMqConfig.java
@@ -1,15 +1,12 @@
 package com.example.notificationservice.notification.config;
 
-import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static org.springframework.amqp.core.Binding.DestinationType.QUEUE;
 
 @Configuration
 public class RabbitMqConfig {
@@ -27,6 +24,7 @@ public class RabbitMqConfig {
     public TopicExchange exchange() {
         return new TopicExchange(EXCHANGE_NAME);
     }
+
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder
@@ -34,4 +32,10 @@ public class RabbitMqConfig {
                 .to(exchange)
                 .with(ROUTING_KEY);
     }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
 }
---
+++ src/main/java/com/example/notificationservice/notification/consumer/NotificationRabbitConsumer.java
@@ -0,0 +1,19 @@
+package com.example.notificationservice.notification.consumer;
+
+import com.example.notificationservice.core.domain.Notification;
+import com.example.notificationservice.core.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationRabbitConsumer {
+
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "notification_queue")
+    public void handleEmail(Notification notification) {
+        notificationService.notify(notification);
+    }
+}
