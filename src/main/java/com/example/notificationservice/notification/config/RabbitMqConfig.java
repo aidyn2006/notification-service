77fb85ec5f80e00ff5de42b416c1cebe1562a762
a.nurlan <a.nurlan@documentolog.com>
2025-07-22 16:43:54 +0500

    Changes

--- src/main/java/com/example/notificationservice/notification/config/RabbitMqConfig.java
+++ src/main/java/com/example/notificationservice/notification/config/RabbitMqConfig.java
@@ -4,7 +4,6 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,10 +33,7 @@ public class RabbitMqConfig {
                 .with(ROUTING_KEY);
     }
 
-//    @Bean
-//    public Jackson2JsonMessageConverter jsonMessageConverter() {
-//        return new Jackson2JsonMessageConverter();
-//    }
+
     @Bean
     public CommandLineRunner runner(Queue queue) {
         return args -> {
